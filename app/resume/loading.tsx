7f2711013c23c982763dff25e3903ac0ca60f98e
Vu Nguyen <nphivu414@gmail.com>
2023-06-26 16:16:43 +0700

    - Posible fix for connection closed issue

--- app/resume/loading.tsx
+++ app/resume/loading.tsx
@@ -1,17 +1,11 @@
-import { AlertDialog, AlertDialogContent, AlertDialogDescription, AlertDialogHeader, AlertDialogTitle } from '@/components/ui/AlertDialog';
+// import { AlertDialog, AlertDialogContent, AlertDialogDescription, AlertDialogHeader, AlertDialogTitle } from '@/components/ui/AlertDialog';
+import { Flex } from '@/components/ui/Flex';
 import React from 'react';
 
 export default function Loading() {
   return (
-    <AlertDialog open>
-      <AlertDialogContent>
-        <AlertDialogHeader>
-          <AlertDialogTitle className='text-center'>Your resume is being generated</AlertDialogTitle>
-          <AlertDialogDescription className='text-center'>
-            We are anylizing your profile data and generating your resume. This may take a few seconds.
-          </AlertDialogDescription>
-        </AlertDialogHeader>
-      </AlertDialogContent>
-    </AlertDialog>
+    <Flex className='flex-1' justify="center" align="center">
+      <p>We are anylizing your profile data and generating your resume. This may take a few seconds.</p>
+    </Flex>
   )
 }
