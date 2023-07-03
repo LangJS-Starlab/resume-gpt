957c18df415cb13bdb0628f17de6df52bffa9160
Vu Nguyen <nphivu414@gmail.com>
2023-07-03 22:55:29 +0700

    - Minor updates

--- app/page.tsx
+++ app/page.tsx
@@ -7,9 +7,6 @@ import Link from "next/link"
 export default async function IndexPage() {
   return (
     <>
-
-      {/*Async Component ts issue: https://github.com/vercel/next.js/issues/42292*/}
-      {/* @ts-expect-error Server Component: */}
       <SiteHeader />
       <section className="space-y-6 pb-8 pt-6 md:pb-12 md:pt-10 lg:py-32">
         <div className="container flex max-w-[64rem] flex-col items-center gap-4 text-center">
