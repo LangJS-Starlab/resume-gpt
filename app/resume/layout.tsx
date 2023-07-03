3d118b8555bb6589dbb7ddb9ce93ea5b23e61f50
Vu Nguyen <nphivu414@gmail.com>
2023-07-03 22:57:58 +0700

    - Fix ts errors

--- app/resume/layout.tsx
+++ app/resume/layout.tsx
@@ -7,8 +7,6 @@ type ResumeLayoutProps = {
 export default function ResumeLayout({ children }: ResumeLayoutProps) {
   return (
     <div>
-      {/*Async Component ts issue: https://github.com/vercel/next.js/issues/42292*/}
-      {/* @ts-expect-error Server Component: */}
       <SiteHeader/>
       {children}
     </div>)
