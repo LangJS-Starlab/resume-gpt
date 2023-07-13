fdc1ea2300d863ca6eb75a0a5e26005ad0aef55e
Vu Nguyen <nphivu414@gmail.com>
2023-07-13 22:22:05 +0700

    - Minor improvements

--- components/modules/resume/ResumePreview.tsx
+++ components/modules/resume/ResumePreview.tsx
@@ -4,6 +4,7 @@ import { Button } from '@/components/ui/Button';
 import { Flex } from '@/components/ui/Flex';
 import { ScrollArea } from '@/components/ui/ScrollArea';
 import { Icons } from '@/components/Icons';
+import { Subtle } from '@/components/ui/Typography';
 
 type ResumePreviewProps = {
   resumeHtmlData?: string
@@ -13,11 +14,11 @@ type ResumePreviewProps = {
 function downloadPdfFromBase64(base64Data: string) {
   const pdfWindow = window.open("")
   pdfWindow?.document.write("<iframe frameBorder='0' width='100%' height='100%' src='"+encodeURI(base64Data)+"'></iframe>")
-  pdfWindow?.document.close();
   setTimeout(() => {
     pdfWindow?.focus();
     pdfWindow?.print(); 
-  }, 100);
+    pdfWindow?.document.close();
+  }, 300);
 }
 
 export const ResumePreview = ({ resumeHtmlData, isLoading }: ResumePreviewProps) => {
@@ -27,6 +28,9 @@ export const ResumePreview = ({ resumeHtmlData, isLoading }: ResumePreviewProps)
       downloadPdfFromBase64(data.data)
     },
   })
+
+  const statusText = isLoading ? 'Saving' : 'Saved'
+  const statusIcon = isLoading ? <Icons.spinner size={16} className="animate-spin text-muted-foreground" /> : <Icons.checkCircle size={16} className="text-muted-foreground"/>
 
   const onPrintButtonClick = () => {
     refetch()
@@ -39,17 +43,16 @@ export const ResumePreview = ({ resumeHtmlData, isLoading }: ResumePreviewProps)
   return (
     <ScrollArea  className="relative h-full w-full">
       <Flex justify="end" className='absolute mb-2 h-10 w-full bg-background shadow-md'>
-        {
-          isLoading ? (
-            <div className='absolute left-0 top-1 mr-2 flex items-center justify-center'>
-              <Icons.spinner size={18} className="animate-spin" />
-              <p className='ml-2 mt-0'>Saving</p>
-            </div>
-          ) : <div className='absolute left-0 top-1 mr-2 flex items-center justify-center'>
-            <Icons.checkCircle size={18} />
-            <p className='ml-2 mt-0'>Saved</p>
-          </div>
-        }
+        <div className='absolute left-0 top-1 mr-2 flex items-center justify-center'>
+          {
+            <Flex className='mt-2' align="center">
+              {statusIcon}
+              <Subtle className='ml-2 mt-0'>
+                {statusText}
+              </Subtle>
+            </Flex>
+          }
+        </div>
         <Button
           variant="outline"
           size="sm"
